67fc563d8882380000d22bcd4c75c03aabd62632
thadeus <thadeusnyariki94@gmail.com>
2023-04-21 14:27:27 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,215 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+homecards = HomeCard.create!([
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/0558/1363/6144/products/PHOTO-2022-10-13-17-06-07-removebg.png?v=1667041431",
+        desc: 'Lets help you pick'
+    },
+    {
+        id: 2,
+        img: "https://mediaservice.audi.com/media/live/50720/fly1400x601n1/4mgax2/2023.png?imwidth=850",
+        desc: "car dealership in  kenya"
+    },
+    {
+        id: 3,
+        img: "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/1eea4b99-e9b7-438a-a4c5-b996989f8ff7/6131ef34-38b4-4acb-8e1a-621dd32a1241.png",
+        desc: 'Home if high end cars'
+    },
+    {
+        id: 4,
+        img: "https://www.mbusa.com/content/dam/mb-nafta/us/homepage-redesign/2024-FVP-CLA-COUPE.jpg",
+        desc: 'premier car mart'
+    },
+    {
+        id: 5,
+        img: "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Land-Rover-Range-Rover-Sport.png",
+        desc: 'the best car deals'
+    },
+])
+
+carmakes = CarMake.create!([
+    {
+        id: 1,
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/LandRover.svg/1200px-LandRover.svg.png',
+        sum: 10
+    },
+    {
+        id: 2,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Mercedes-Logo.svg/2048px-Mercedes-Logo.svg.png',
+        sum: 10
+    },
+    {
+        id: 3,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/2048px-BMW.svg.png',
+        sum: 10
+    },
+    {
+        id: 4,
+        logo: 'https://upload.wikimedia.org/wikipedia/sco/thumb/f/fe/Subaru_logo_and_wordmark.svg/1280px-Subaru_logo_and_wordmark.svg.png',
+        sum: 10
+    },
+    {
+        id: 5,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Mitsubishi_logo.svg/2381px-Mitsubishi_logo.svg.png',
+        sum: 10
+    },
+    {
+        id: 6,
+        logo: 'https://upload.wikimedia.org/wikipedia/fr/1/15/Audi_logo.svg',
+        sum: 10
+    },
+    {
+        id: 7,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Jeep_logo.svg',
+        sum: 10
+    },
+    {
+        id: 8,
+        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Lexus_division_emblem.svg/2560px-Lexus_division_emblem.svg.png',
+        sum: 10
+    },
+    {
+        id: 9,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Honda.svg/2560px-Honda.svg.png',
+        sum: 10
+    },
+    {
+        id: 10,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Volkswagen_logo_2019.svg/2048px-Volkswagen_logo_2019.svg.png',
+        sum: 10
+    },
+    {
+        id: 10,
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Toyota_EU.svg/800px-Toyota_EU.svg.png',
+        sum: 10
+    },
+])
+
+cars = Car.create!([
+    {   
+        id: 1,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/2016_Toyota_HiLux_Invincible_D-4D_4WD_2.4_Front.jpg',
+        desc: '2016 Toyota Hilux',
+        price: '5,900,000',
+        engine: '2,400',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+    {
+        id: 2,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Toyota_HARRIER_G_4WD.jpg/1024px-Toyota_HARRIER_G_4WD.jpg',
+        desc: '2014 Toyota Harrier',
+        price: '3,200,000',
+        engine: '2,000',
+        transmission: 'auto',
+        fuel: 'petrol'
+    },
+    {
+        id: 3,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/5/5d/2018_Subaru_Impreza_%28GT7%29_2.0i-S_hatchback_%282018-09-28%29_01.jpg',
+        desc: '2014 subaru impreza',
+        price: '1,300,000',
+        engine: '1,600',
+        transmission: 'auto',
+        fuel: 'petrol'
+    },
+    {
+        id: 4,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/2018_Toyota_Land_Cruiser_Prado_%28GDJ150R%29_GXL_wagon_%282018-08-06%29_01.jpg',
+        desc: '2018 toyota land cruiser prado',
+        price: '6,900,000',
+        engine: '2,800',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+    {
+        id: 5,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Mitsubishi_V98_Pajero_Long_Body_Super_Exceed_3200_DI-D.JPG',
+        desc: '2018 mitsubushi pajero',
+        price: '5,900,000',
+        engine: '3,200',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+    {
+        id: 6,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/2021_Toyota_Land_Cruiser_300_3.4_ZX_%28Colombia%29_front_view_04.png',
+        desc: '2021 land cruiser',
+        price: '6,100,000',
+        engine: '2,800',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+    {
+        id: 7,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/2021_Toyota_Land_Cruiser_300_3.4_ZX_%28Colombia%29_front_view_04.png',
+        desc: '2018 land cruiser',
+        price: '6,800,000',
+        engine: '2,800',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+    {
+        id: 8,
+        img: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/2021_Toyota_Land_Cruiser_300_3.4_ZX_%28Colombia%29_front_view_04.png',
+        desc: '2022 land cruiser',
+        price: '13,500,000',
+        engine: '4,500',
+        transmission: 'auto',
+        fuel: 'diesel'
+    },
+])
+
+accessories = Accessory.create!([
+    {
+        id: 1,
+        img: 'https://www.autoventshade.com/cache/images_product-images_AVS760x500Windflector1_w760_h500_cr_q75.jpg',
+        title: 'Sunroof Wind Deflector',
+        desc: 'Makes driving with the sun roof open even more pleasurable by reducing the effects of wind noise and turbulence within the cabin.'
+    },
+    {
+        id: 2,
+        img: 'https://www.autoventshade.com/cache/images_product-images_LightshieldPro_Lightshieldpro_760x500_w365_h200_cr_q80.jpg',
+        title: 'Hood Deflector',
+        desc: 'Ideal for bad weather and country road driving. The deflector fits along the leading edge of the hood to divert airborne water, mud and stones away from the windshield.'
+    },
+    {
+        id: 3,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Wind-deflectors.jpg',
+        title: 'Sunroof Wind Deflector',
+        desc: 'The aerodynamic shape minimizes in-cabin wind noise and turbulence when driving with the windows open. Available as a front and rear set or for the front only.'
+    },
+    {
+        id: 4,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Door-handle-protection-film.jpg',
+        title: 'Door Handle Protection Film',
+        desc: 'Virtually invisible, yet providing tough protection against scratching by gloves, rings and keys around the door handle.'
+    },
+    {
+        id: 5,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Touch-up-paints.jpg',
+        title: 'Touch Up Paints',
+        desc: 'A full range of Land Cruiser colors available in easy to use sticks and aerosols for repairing minor paintwork blemishes.'
+    },
+    {
+        id: 6,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Cool-box.jpg',
+        title: 'Cool Box',
+        desc: 'Ideal for long journeys, picnics and days out. Plugs into your car’s 12V socket to keep refreshments either hot or chilled.'
+    },
+    {
+        id: 7,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Alloy-wheel-ProTect.jpg',
+        title: 'Alloy Wheel ProTect',
+        desc: 'ProTect’s silicium-based coating shields alloy wheel surfaces against marks and grime caused by brake dust, road conditions and the weather.'
+    },
+    {
+        id: 8,
+        img: 'https://www.carsoko.co.ke/frontend-assets/img/Velour-floor-mats.jpg',
+        title: 'Velour Floor Mats',
+        desc: 'Soft textile on a strong backing for great looks, comfort and durable carpet protection. Safety fixings hold the driver’s mat firmly in place. In beige or blue grey and for 5 and 7 seat Land Cruisers.'
+    },
+])
